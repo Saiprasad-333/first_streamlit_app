99560fb2a65adbef104a2e6db086d4c2ac8295e3
Saiprasad-333 <123240896+Saiprasad-333@users.noreply.github.com>
2023-01-25 15:28:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,9 +57,6 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 
 
 
-my_cur.execute("my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+
 my_cur.execute("insert into fruit_load_list values('from streamlit')")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe("my_data_rows")
+
